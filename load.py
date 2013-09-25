6ed1c96bdb1f0967d978090acabe1f49350a0295
Waiting <waiting0819@msn.com>
2013-09-25 13:16:30 +0800

    Add Load.py and config.py

---
+++ load.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*- 
+
+import os
+
+# execfile(os.getcwd() + "/config/config.py")
+
+exec(open(os.getcwd() + "/config/config.py").read())
+
+if len(sys.argv) < 2:
+    print ('please input card_set')
+    exit()
+    
+if sys.argv[1] not in card_set:
+    print ('please input supportted card_set')
+    print (card_set)
+    exit()
+    
+set = sys.argv[1]
+
+if os.path.exists("card_data") == False:
+    os.mkdir("card_data")
+
+if os.path.exists("card_data/%s"%(set)) == False:
+    os.mkdir("card_data/%s"%(set))
+
+for lan in language:    
+    if os.path.exists("card_data/%s/%s"%(set, lan)) == False:    
+        os.mkdir("card_data/%s/%s"%(set, lan))
+
+    if os.path.exists("card_data/%s/%s_%s_all.html"%(set, set, lan)) == False:
+    
+        f = urllib.request.urlopen("http://magiccards.info/%s/%s.html"%(set, lan))
+        line = f.read()
+        
+        w = open("card_data/%s/%s_%s_all.html"%(set, set, lan), "w")
+        w.write(line.decode('cp950','ignore'))
+        print ("%s/%s_%s_all.html"%(set, set, lan))
+        
+        f.close()
+        w.close()
+        
+    for i in range(total_num[set]):
+    
+        index = i + 1
+        print (index)
+        
+        # isd dka
+        if set == 'isd' or set == 'dka':
+            
+            if index in flip[set]:
+
+                if os.path.exists("card_data/%s/%s/%da.html"% (set, lan, index)) == False:
+            
+                    fa = urllib.request.urlopen("http://magiccards.info/%s/%s/%da.html" % (set, lan, index))
+                    fb = urllib.request.urlopen("http://magiccards.info/%s/%s/%db.html" % (set, lan, index))
+                    
+                    linea = fa.read()
+                    lineb = fb.read()
+                    
+                    wa = open("card_data/%s/%s/%da.html"% (set, lan, index), "w")
+                    wb = open("card_data/%s/%s/%db.html"% (set, lan, index), "w")
+                    
+                    wa.write(linea.decode('cp950','ignore'))
+                    wb.write(lineb.decode('cp950','ignore'))
+                
+                    print ('save %s %d.html'%(set, index))
+                    
+                    fa.close()
+                    fb.close()
+                
+                    wa.close()
+                    wb.close()
+                    
+            else:
+            
+                if os.path.exists("card_data/%s/%s/%d.html"% (set, lan, index)) == False:
+                
+                    f = urllib.request.urlopen("http://magiccards.info/%s/%s/%d.html" % (set, lan, index))
+                        
+                    line = f.read()
+                            
+                    w = open("card_data/%s/%s/%d.html"% (set, lan, index), "w")
+                    w.write(line.decode('cp950','ignore'))
+                        
+                    print ('save %s %d.html'%(set, index))
+                                
+                    f.close()
+                    w.close()
+                    
+        else:
+            if os.path.exists("card_data/%s/%s/%d.html"% (set, lan, index)) == False:
+                
+                f = urllib.request.urlopen("http://magiccards.info/%s/%s/%d.html" % (set, lan, index))
+                    
+                line = f.read()
+                        
+                w = open("card_data/%s/%s/%d.html"% (set, lan, index), "w")
+                w.write(line.decode('cp950','ignore'))
+                    
+                print ('save %s %d.html'%(set, index))
+                            
+                f.close()
+                w.close()
+                time.sleep(1)
